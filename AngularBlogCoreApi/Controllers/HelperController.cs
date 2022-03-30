1cbb4ccb1a3a514dc2e92c1a7ea26e4fbe8fdb2f
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-03-31 02:49:04 +0300

    Helper Email send was done.

---
+++ AngularBlogCoreApi/Controllers/HelperController.cs
@@ -0,0 +1,53 @@
+﻿using AngularBlogCoreApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace AngularBlogCoreApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HelperController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendContactEmail(Contact contact)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                NetworkCredential kullanicibilgi = new NetworkCredential("cyptometa@gmail.com", "kripto10");
+                smtp.Credentials = kullanicibilgi;
+
+                MailAddress gonderen = new MailAddress("cyptometa@gmail.com");
+
+                MailAddress alici = new MailAddress("bahtiyaregemen51@gmail.com");
+
+                MailMessage mail = new MailMessage(gonderen, alici);
+                mail.Subject = contact.Subject;
+                mail.Body = contact.Message;
+                mail.IsBodyHtml = true;
+                smtp.Send(mail);
+
+                return Ok();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+
+
+        }
+    }
+}
---
+++ AngularBlogCoreApi/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularBlogCoreApi.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+    
+}
