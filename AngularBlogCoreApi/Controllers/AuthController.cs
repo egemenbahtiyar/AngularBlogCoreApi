51f7d5ad9c98dee6a7a33c1f343acaaec5eb69af
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-08 22:22:26 +0300

    Authentication was involved.

---
+++ AngularBlogCoreApi/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using AngularBlogCoreApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularBlogCoreApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult IsAuthenticated(AdminUser adminUser)
+        {
+            bool status = false;
+            if (adminUser.Email == "cyptometa@gmail.com" && adminUser.Password == "1235")
+            {
+                status = true;
+            }
+            var result = new {
+                status = status
+            };
+            return Ok(result);
+        }
+    }
+}
